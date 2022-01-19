768dca335f2029b67237287fa1e7e38e84faba2c
Baptista Neves <baptistafirminoneves@gmail.com>
2022-01-19 21:26:06 +0100

    Primeiro commit, Setup da aplicação

---
+++ src/SV.Core/Entities/Base/Entidade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SV.Core.Entities.Base
+{
+    public abstract class Entidade
+    {
+        public Guid Id { get; set; }
+        public Entidade()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/SV.Core/Interfaces/Repositories/Generic/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using SV.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SV.Core.Interfaces.Repositories.Generic
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entidade
+    {
+    }
+}
